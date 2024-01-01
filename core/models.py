4533ce8fe7d40c5f9d919bce072a8bafe4535d7a
rafitalavi <rafitalavi12345@gmail.com>
2024-01-01 21:04:15 +0600

    first commit

---
+++ core/models.py
@@ -0,0 +1,33 @@
+import datetime
+from time import time
+
+from django.db import models
+
+
+
+
+types = [('employee','employee'),('visitor','visitor')]
+class Profile(models.Model):
+    first_name = models.CharField(max_length=70)
+    last_name = models.CharField(max_length=70)
+    date = models.DateField()
+    phone = models.BigIntegerField()
+    email = models.EmailField()
+    # ranking = models.IntegerField(null=True)
+    profession = models.CharField(max_length=200)
+    # status = models.CharField(choices=types,max_length=20,null=True,blank=False,default='employee')
+    # dept = models.CharField(choices=depta,max_length=20,null=True,blank=False,default='Select Department')
+    present = models.BooleanField(default=False)
+    image = models.ImageField()
+    updated = models.DateTimeField(auto_now=True)
+    #shift = models.TimeField(null=True)
+    def __str__(self):
+        return self.first_name +' '+self.last_name
+
+
+class LastFace(models.Model):
+    last_face = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.last_face
+
